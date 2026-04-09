5c6acefc6f5c44d42207cb392e3b66c705188e6b
timak roman <80565265+roman-boop@users.noreply.github.com>
2026-04-10 02:51:25 +0600

    Update HeroSection.tsx

--- cryptomaniac/src/app/homepage/components/HeroSection.tsx
+++ cryptomaniac/src/app/homepage/components/HeroSection.tsx
@@ -46,7 +46,7 @@ const SOCIAL_LINKS = [
   },
   {
     label: 'Rutube',
-    href: 'https://rutube.ru',
+    href: 'https://rutube.ru/channel/18741535/',
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" />
